c0391b113233948d0cf3aae0f0d8551575bd585d
Воробьёв Константин ИП-516 <kostya_vorobyov@inbox.ru>
2025-10-14 16:37:23 +0700

    4

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
-    return 0;
+int main(int argc, char *argv[]) {
+    int a[10];
+    int x = atoi(argv[11]);
+    int k = atoi(argv[12]);
+    for (int i = 0; i <= 9; ++i) {
+        a[i] = atoi(argv[i+1]);
+    }
+
+    for (int i = 9; i > k; --i) {
+        a[i] = a[i-1];
+    }
+    a[k] = x;
+    for (int i = 0; i <= 9; ++i) {
+        printf("%d ", a[i]);
+    }
 }
