8a9810db3f2ac91705b4b5f8f9863724a465ce4c
Воробьёв Константин ИП-516 <kostya_vorobyov@inbox.ru>
2025-10-14 16:32:16 +0700

    1

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
-    return 0;
+int main(int argc, char *argv[]) {
+    int a[10];
+    for (int i = 0; i <= 9; ++i) {
+        a[i] = atoi(argv[i+1]);
+    }
+    float sr_arif = 0;
+    
+    for (int i = 0; i<=9; ++i) {
+        sr_arif += a[i];
+    }
+    printf("%.2f\n", sr_arif/10);
 }
