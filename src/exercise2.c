b7bd2b0aac83719765e7b0fae09a1e85a763381a
Воробьёв Константин ИП-516 <kostya_vorobyov@inbox.ru>
2025-10-15 11:45:00 +0700

    2

--- src/exercise2.c
+++ src/exercise2.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    int a[10];
+    double a[10];
     for (int i = 0; i <= 9; ++i) {
+        scanf("%lf", &a[i]);
         a[i] = atoi(argv[i+1]);
     }
     for (int i = 9; i >= 0; --i) {
