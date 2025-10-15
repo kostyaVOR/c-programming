79a3fb97caff77663fdfabfda83a1b11ca09010e
Воробьёв Константин ИП-516 <kostya_vorobyov@inbox.ru>
2025-10-15 11:58:40 +0700

    3

--- src/exercise3.c
+++ src/exercise3.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    double a[10];
-    int k = scanf("%d", &k);
+    int a[10];
+    int k = atoi(argv[11]);
     for (int i = 0; i <= 9; ++i) {
-        scanf("%lf", &a[i+1]);
+        a[i] = atoi(argv[i+1]);
     }
     if (k > 0) {
         for (int i = 0; i <= 9; ++i) {
-            printf("%f " , a[(i - k + 10) % 10]);
+            printf("%d " , a[(i - k + 10) % 10]);
         }
     }
     else if (k < 0) {
         for (int i = 0; i <= 9; ++i) {
-            printf("%f ", a[(i - k) % 10]);
+            printf("%d ", a[(i - k) % 10]);
         }
     }
 }
