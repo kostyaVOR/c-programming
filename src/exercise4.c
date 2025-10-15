a71403c7fe885e9d74c11acc8df3a5435179448f
Воробьёв Константин ИП-516 <kostya_vorobyov@inbox.ru>
2025-10-15 12:02:33 +0700

    4

--- src/exercise4.c
+++ src/exercise4.c
@@ -4,15 +4,15 @@
 int main(int argc, char *argv[]) {
     int a[10];
     int x = atoi(argv[11]);
-    int k = atoi(argv[12]);
+    int per = atoi(argv[12]);
     for (int i = 0; i <= 9; ++i) {
         a[i] = atoi(argv[i+1]);
     }
 
-    for (int i = 9; i > k; --i) {
+    for (int i = 9; i > per; --i) {
         a[i] = a[i-1];
     }
-    a[k] = x;
+    a[per] = x;
     for (int i = 0; i <= 9; ++i) {
         printf("%d ", a[i]);
     }
