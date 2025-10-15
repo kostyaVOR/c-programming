669c7efe3b4d3e00e39bdd522c9aa09eb22e2c9f
Воробьёв Константин ИП-516 <kostya_vorobyov@inbox.ru>
2025-10-15 10:14:23 +0700

    1

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
     float a[10];
     for (int i = 0; i < 10; ++i) {
-        a[i] = atof(argv[i+1]);
+        scanf("%f", &a[i]);
     }
     float sum = 0.0;
     
