d419bf3e0cf2c8900e982003f7833d786d4e7de9
Воробьёв Константин ИП-516 <kostya_vorobyov@inbox.ru>
2025-10-15 11:15:42 +0700

    1

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include <math.h>
 
-int main() {
-    double a[10];
-    double sum = 0;
-    for (int i = 0; i < 10; ++i) {
+int main(int argc, char** argv) {
+    double a[10]={};
+    for (int i = 0; i < 10; i++) {
         scanf("%lf", &a[i]);
-        sum += round(a[i] * 100.0);
     }
-    double sr_arif = sum / 1000.0;
-    printf("%.2f\n", sr_arif);
+
+    float sum = 0;
+    for (int i = 0; i < 10; i++) {
+        sum += a[i];
+    }
+    printf("%f\n", sum/10);
     return 0;
 }
