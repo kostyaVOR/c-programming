746d9fa1ecded7bad5874b02bcefb90e613c50c5
Воробьёв Константин ИП-516 <kostya_vorobyov@inbox.ru>
2025-10-22 17:27:51 +0700

    4

--- src/exercise4.c
+++ src/exercise4.c
@@ -2,18 +2,20 @@
 
 int main() {
     double a[10];
-    double X;
+    double x;
     int per;
 
     for (int i = 0; i < 10; i++) {
         scanf("%lf", &a[i]);
     }
-    scanf("%lf %d", &X, &per);
+    scanf("%lf", &x);
+    scanf("%d", &per);
     for (int i = 9; i > per; i--) {
         a[i] = a[i - 1];
     }
-    a[per] = X;
+    a[per] = x;
+
     for (int i = 0; i < 10; i++) {
-        printf("%lf", a[i]);
+        printf("%lf\n", a[i]);
     }
 }
