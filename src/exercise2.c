1bc0b069468328eaf01d7f5eae503922dc6b516e
Воробьёв Константин ИП-516 <kostya_vorobyov@inbox.ru>
2025-10-22 16:26:02 +0700

    2

--- src/exercise2.c
+++ src/exercise2.c
@@ -4,9 +4,8 @@ int main(int argc, char *argv[]) {
     double a[10];
     for (int i = 0; i <= 9; ++i) {
         scanf("%lf", &a[i]);
-        a[i] = atoi(argv[i+1]);
     }
     for (int i = 9; i >= 0; --i) {
-        printf("%d ", a[i]);
+        printf("%lf ", a[i]);
     }
 }
