801181114aaeda948bed82ffb6af7e0460158513
Воробьёв Константин ИП-516 <kostya_vorobyov@inbox.ru>
2025-10-22 17:48:25 +0700

    5

--- src/exercise5.c
+++ src/exercise5.c
@@ -1,32 +1,37 @@
 #include <stdio.h>
 
 int main() {
-    double matrix1[10][3];
-    double matrix2[3][10];
-    double result[10][10];
+    double a[10][3];
+    double b[3][10];
+    double c[10][10];
 
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 3; j++) {
-            scanf("%lf", &matrix1[i][j]);
+            scanf("%lf", &a[i][j]);
         }
     }
+
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 10; j++) {
-            scanf("%lf", &matrix2[i][j]);
+            scanf("%lf", &b[i][j]);
         }
     }
+
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            result[i][j] = 0.0;
+            c[i][j] = 0.0;
             for (int k = 0; k < 3; k++) {
-                result[i][j] += matrix1[i][k] * matrix2[k][j];
+                c[i][j] += a[i][k] * b[k][j];
             }
         }
     }
+
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            printf("%.2f", result[i][j]);
+            if (j > 0) printf(" ");
+            printf("%.2f", c[i][j]);
         }
+        printf("\n");
     }
 
     return 0;
