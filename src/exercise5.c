7724670e2df3432a60995df3366995934724f125
Воробьёв Константин ИП-516 <kostya_vorobyov@inbox.ru>
2025-10-22 17:16:13 +0700

    5

--- src/exercise5.c
+++ src/exercise5.c
@@ -23,6 +23,7 @@ int main() {
     }
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-            printf("%lf", C[i][j]))
+            printf("%lf", C[i][j]);
+    }
     }
 }
