3f6d09a9de26966c6c4fd163d214ef5882293547
Воробьёв Константин ИП-516 <kostya_vorobyov@inbox.ru>
2025-10-22 17:14:18 +0700

    4

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+int main() {
     double a[10];
-    double x;
+    double X;
     int per;
-    for (int i = 0; i <= 9; ++i) {
+
+    for (int i = 0; i < 10; i++) {
         scanf("%lf", &a[i]);
     }
-    scanf("%lf %d", &x, &per);
-    a[per] = x;
-    for (int i = 9; i > per; --i) {
-        a[i] = a[i-1];
+    scanf("%lf %d", &X, &per);
+    for (int i = 9; i > per; i--) {
+        a[i] = a[i - 1];
     }
-    for (int i = 0; i <= 9; ++i) {
-        printf("%lf ", a[i]);
+    a[per] = X;
+    for (int i = 0; i < 10; i++) {
+        printf("%lf", a[i]);
     }
 }
